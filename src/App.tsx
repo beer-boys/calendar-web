f7b141221130628897e1c879b27fb7f0257c7cfb
Максим Давыдов <maxim.davydov@vkteam.ru>
2025-11-07 01:46:21 +0300

    Статичная версия для показа

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import { Epic, SplitCol, SplitLayout } from '@vkontakte/vkui';
+import ruLocale from '@fullcalendar/core/locales/ru';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/react';
+import { Icon24GearOutline } from '@vkontakte/icons';
+import { Button, Calendar, Checkbox, FormLayoutGroup } from '@vkontakte/vkui';
 
-import { Main } from './main/Main';
-const MAIN_VIEW_ID = 'main-view';
+import styles from './App.module.css';
 
 export function App() {
   return (
-    <SplitLayout>
-      <SplitCol stretchedOnMobile autoSpaced>
-        <Epic activeStory={MAIN_VIEW_ID}>
-          <Main id={MAIN_VIEW_ID} />
-        </Epic>
-      </SplitCol>
-    </SplitLayout>
+    <div className={styles.root}>
+      <div className={styles.sideBar}>
+        <div className={styles.logo}>
+          <span className={styles.logoFirstLetter}>X</span>-Calendar
+        </div>
+        <Button size="l">+ Создать</Button>
+        <Calendar />
+        <FormLayoutGroup mode="vertical">
+          <Checkbox name="meetings" defaultChecked>
+            Встречи
+          </Checkbox>
+          <Checkbox name="habbits" defaultChecked>
+            Привычки
+          </Checkbox>
+        </FormLayoutGroup>
+        <div className={styles.iconWrapper}>
+          <Icon24GearOutline />
+        </div>
+      </div>
+      <div className={styles.calendarWrapper}>
+        <FullCalendar
+          locale={ruLocale}
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          weekends={false}
+          events={[{ title: 'Конец спринта 1', date: '2025-11-07' }]}
+        />
+      </div>
+    </div>
   );
 }
