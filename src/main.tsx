d0f00244b0a2568e5cd99c1797130d5baa58261c
Максим Давыдов <maxim.davydov@vkteam.ru>
2025-11-20 19:24:40 +0300

    Добавил redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import '@vkontakte/vkui/dist/vkui.css';
 import { AppRoot, ConfigProvider } from '@vkontakte/vkui';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import { App } from './App.tsx';
+import { store } from './store/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ConfigProvider>
       <AppRoot>
-        <App />
+        <ReduxProvider store={store}>
+          <App />
+        </ReduxProvider>
       </AppRoot>
     </ConfigProvider>
   </StrictMode>,
