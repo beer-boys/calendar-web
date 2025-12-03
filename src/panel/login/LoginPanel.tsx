3217ca028d488c6e55196e124d5654f5e24a8631
Максим Давыдов <maxim.davydov@vkteam.ru>
2025-12-04 02:56:40 +0300

    Поправил loginpanel

--- src/panel/login/LoginPanel.tsx
+++ src/panel/login/LoginPanel.tsx
@@ -1,5 +1,7 @@
-import { Button, Flex, Panel } from '@vkontakte/vkui';
+import { Button, ButtonGroup, Flex, Panel } from '@vkontakte/vkui';
+import { useDispatch } from 'react-redux';
 
+import { MODALS, openModal } from '@/modules/modal/modal.reducer';
 import { Logo } from '@/ui/Logo/Logo';
 
 import styles from './LoginPanel.module.css';
@@ -9,15 +11,29 @@ interface LoginPanelProps {
 }
 
 export function LoginPanel({ id }: LoginPanelProps) {
+  const dispatch = useDispatch();
+
+  const onLoginClick = () => {
+    dispatch(openModal({ modalId: MODALS.login }));
+  };
+
+  const onRegistationClick = () => {
+    dispatch(openModal({ modalId: MODALS.registration }));
+  };
+
   return (
     <Panel id={id}>
       <Flex className={styles.wrapper} direction="column" justify="center" align="center">
-        <Flex className={styles.content} direction="column" align="stretch" gap="l">
-          <Flex direction="column" align="center">
-            <Logo />
-          </Flex>
-          <Button mode="primary">Войти</Button>
-          <Button mode="outline">Зарегистрироваться</Button>
+        <Flex className={styles.content} direction="column" align="center" gap="l">
+          <Logo />
+          <ButtonGroup stretched gap="s" mode="vertical">
+            <Button mode="primary" size="l" onClick={onLoginClick} stretched>
+              Войти
+            </Button>
+            <Button mode="outline" size="l" onClick={onRegistationClick} stretched>
+              Зарегистрироваться
+            </Button>
+          </ButtonGroup>
         </Flex>
       </Flex>
     </Panel>
