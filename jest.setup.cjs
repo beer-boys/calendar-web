090aedbc45cf86a82be03759a029b64dc930020f
Максим Давыдов <maxim.davydov@vkteam.ru>
2026-01-20 23:35:55 +0300

    Добавил тест на распахивание кнопки создания

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -1,3 +1,21 @@
 require('@testing-library/jest-dom');
 
 global.__VITE_API_URL__ = 'http://localhost:3000';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+
+    // современные API
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+
+    // старые API (некоторые либы до сих пор используют)
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  }),
+});
--- src/ui/CreateButton/__test__/CreateButton.test.tsx
+++ src/ui/CreateButton/__test__/CreateButton.test.tsx
@@ -1,3 +1,6 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { renderWithStore } from '@/test/renderWithStore';
 import { CreateButton } from '@/ui/CreateButton/CreateButton';
 
@@ -5,5 +8,19 @@ describe('CreateButton', () => {
   it('should render', () => {
     const rendered = renderWithStore(<CreateButton />);
     expect(rendered.asFragment()).toMatchSnapshot();
+  });
+
+  const creatableItems = ['Встречу', 'Привычку'];
+  it('should expand', async () => {
+    const user = userEvent.setup();
+    const { container } = renderWithStore(<CreateButton />);
+
+    const createBtn = container.querySelector('[data-qa-id="event-create-bttn"]');
+    expect(createBtn).not.toBeNull();
+
+    await user.click(createBtn!);
+    for (const item of creatableItems) {
+      expect(await screen.findByText(item)).toBeInTheDocument();
+    }
   });
 });
